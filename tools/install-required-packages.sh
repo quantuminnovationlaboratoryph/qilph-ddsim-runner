1eabfe65a2df40a3df29841338f443f98bf81e47
rentristandelacruz <rentristandelacruz@gmail.com>
2023-12-04 12:44:37 +0800

    Updated 'install-required-packages.sh' script.

--- tools/install-required-packages.sh
+++ tools/install-required-packages.sh
@@ -1,13 +1,77 @@
-pip3 install         cotengra==0.5.2
-pip3 install          kahypar==1.3.4
-pip3 install  memory_profiler==0.61.0
-pip3 install        mqt.ddsim==1.20.1
-pip3 install       opt-einsum==3.3.0
-pip3 install         projectq==0.8.0
-pip3 install           qiskit==0.44.2
-pip3 install       qiskit-aer==0.12.2
-pip3 install     qiskit-terra==0.25.2.1
-pip3 install            quimb==1.6.0
-pip3 install           sparse==0.14.0
+echo ""
+echo "====================================================================="
+echo "Installing package [contengra]                  "
+echo "====================================================================="
+pip3 install cotengra
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [kahypar]                    "
+echo "====================================================================="
+pip3 install kahypar
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [memory_profiler]            "
+echo "====================================================================="
+pip3 install memory_profiler  
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [mqt.ddsim]                  "
+echo "====================================================================="
+pip3 install mqt.ddsim
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [opt-einsum]                 "
+echo "====================================================================="
+pip3 install opt-einsum
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [projectq]                   "
+echo "====================================================================="
+pip3 install projectq
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [qiskit]                     "
+echo "====================================================================="
+pip3 install qiskit
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [qiskit-aer]                 "
+echo "====================================================================="
+pip3 install qiskit-aer
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [qiskit-terra]               "
+echo "====================================================================="
+pip3 install qiskit-terra
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [quimb]                      "
+echo "====================================================================="
+pip3 install quimb
+
+
+echo ""
+echo "====================================================================="
+echo "Installing package [sparse]                     "
+echo "====================================================================="
+pip3 install sparse
 
 
